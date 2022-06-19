13661b83204a33646abd3ec22468ea47dab87a4f
SaharshPatel24 <srpatel56@myseneca.ca>
2022-06-19 05:21:52 -0400

    added CSS'

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(express.static(`${__dirname}/public`));
+app.use(express.static(__dirname + '/public'));
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true
