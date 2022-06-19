1ed85ca6450ba3e83932e00716ef8381b577ca71
SaharshPatel24 <srpatel56@myseneca.ca>
2022-06-19 05:11:53 -0400

    Completed

--- index.js
+++ index.js
@@ -1,24 +1,121 @@
-require('dotenv').config();
+const port = 3000;
+
+
 const express = require('express');
-const cors = require('cors');
+require('dotenv').config();
+const dns = require('dns');
+const bodyParser = require('body-parser');
+const mongodb = require('mongodb');
+const mongoose = require('mongoose');
+
 const app = express();
 
-// Basic Configuration
-const port = process.env.PORT || 3000;
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(cors());
+p.use(express.static(`${__dirname}/public`));
 
-app.use('/public', express.static(`${process.cwd()}/public`));
-
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+const { Schema } = mongoose;
+
+const urlSchema = new Schema({
+  original_url: {
+    type: String,
+    required: true
+  },
+  short_url: {
+    type: Number,
+    required: true,
+    default: 0
+  }
 });
 
-app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
+const Url = mongoose.model('Url', urlSchema);
+
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/views/index.html`);
 });
+
+app.post('/api/shorturl', (req, res) => {
+
+  const urlRequest = req.body.url;
+
+  const hostname = urlRequest
+    .replace(/http[s]?\:\/\//, '')
+    .replace(/\/(.+)?/, '');
+
+  dns.lookup(hostname, (lookupErr, addresses) => {
+    if (lookupErr) {
+      console.log('lookup() error');
+    }
+
+    if (!addresses) {
+      res.json({
+        error: 'invalid URL'
+      });
+    } else {
+
+      Url.findOne({
+        original_url: urlRequest
+      }, (findOneErr, urlFound) => {
+        if (findOneErr) {
+          console.log('findOne() error');
+        }
+
+        if (!urlFound) {
+          Url.estimatedDocumentCount((countErr, count) => {
+            if (countErr) {
+              res.send('estimatedDocumentCount() error');
+            }
+
+            const url = new Url({
+              original_url: urlRequest,
+              short_url: count + 1
+            });
+
+            url.save((saveErr, urlSaved) => {
+              if (saveErr) {
+                res.send('save() error');
+              }
+              res.json({
+                original_url: urlSaved.original_url,
+                short_url: urlSaved.short_url
+              });
+            });
+          });
+        } else {
+
+          res.json({
+            original_url: urlFound.original_url,
+            short_url: urlFound.short_url
+          });
+        }
+      });
+    }
+  });
+});
+
+app.get('/api/shorturl/:shorturl', (req, res) => {
+  const { shorturl } = req.params;
+
+  Url.findOne({
+    short_url: shorturl
+  }, (err, urlFound) => {
+    if (err) {
+      console.log('findOne() error');
+    }
+
+    if (!urlFound) {
+      res.json({
+        error: 'no matching URL'
+      });
+    } else {
+      res.redirect(urlFound.original_url);
+    }
+  });
+});
+
+app.listen(port);
+console.log(`listening on port ${port}`);
